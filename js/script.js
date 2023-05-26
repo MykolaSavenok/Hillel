d0987f570916ce2b867317091c418ccc6237e9a0
SavenokNikolay <savenok.kolya.nikolay@gmail.com>
2023-05-26 13:42:19 +0300

    add js

--- js/script.js
+++ js/script.js
@@ -9,3 +9,16 @@ if (!time) {
    alert(`You enter ${time * result} seconds`);
    console.log(`User enter ${time * result} seconds`);
 }
+///////////////////////////////////////////////////////
+const hours = +prompt('Введіть кількість годин');
+if (!hours) {
+   alert('Invalid result');
+   console.log('Invalid result');
+} else {
+   const minutes = 60;
+   const seconds = 60;
+   const secondsInHours = minutes * seconds;
+   alert(`You enter ${hours * secondsInHours} seconds`);
+   console.log(`User enter ${hours * secondsInHours} seconds`);
+}
+/////////////////////////////////////////////////////////
